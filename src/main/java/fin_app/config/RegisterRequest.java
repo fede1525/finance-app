95ab6d73494d8641260bb1392eac65b138e795a2
Federico Resano <123256603+fede1525@users.noreply.github.com>
2024-10-13 19:47:25 -0300

    Add files via upload

---
+++ src/main/java/fin_app/config/RegisterRequest.java
@@ -0,0 +1,10 @@
+package fin_app.config;
+
+public class RegisterRequest {
+
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+
+}
--- src/main/java/fin_app/model/User.java
+++ src/main/java/fin_app/model/User.java
@@ -66,6 +66,9 @@ public class User implements UserDetails {
     }
 
     @Override
+    public String getPassword() { return password; }
+
+    @Override
     public boolean isAccountNonExpired() {
         return true;
     }
